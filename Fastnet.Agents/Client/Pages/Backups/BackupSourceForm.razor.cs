8a7ff818f1383a7e8a02712dacef8c408a23935b
asimshah <asimshah@hotmail.com>
2021-08-05 13:51:25 +0100

    work-ini-progress

---
+++ Fastnet.Agents/Client/Pages/Backups/BackupSourceForm.razor.cs
@@ -0,0 +1,102 @@
+﻿using Fastnet.Agents.Shared;
+using Fastnet.Blazor.Controls;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fastnet.Agents.Client.Pages.Backups
+{
+    public partial class BackupSourceForm
+    {
+        private MessageBox mb;
+        private Dialogue<BackupSourceFolderDTO> dialogue;
+        [Parameter] public BackupSourceFolderDTO Model { get; set; } = new();
+        [Parameter] public OwnerDTO Owner { get; set; }
+        private IEnumerable<DriveDTO> drives { get; set; } = Enumerable.Empty<DriveDTO>();
+        //private IEnumerable<string> driveFolders { get; set; } = Enumerable.Empty<string>();
+        private int driveIndex = 0;
+        //private string selectedFolder;
+        //public void SetModel(BackupSourceFolderDTO model)
+        //{
+        //    Model = model;
+        //    var modelDrive = drives.FirstOrDefault(x => string.Compare(x.VolumeLabel, Model.BackupDriveLabel, true) == 0);
+        //    if(modelDrive != null)
+        //    {
+        //        driveIndex = drives.
+        //    }
+        //}
+        public async Task ShowAsync(BackupSourceFolderDTO sf,  Action<DialogResult> onClose)
+        {
+            var war = await backupService.GetDrives();
+            if (war.Success)
+            {
+                sf ??= new();
+                Model = sf;
+                drives = war.Data;
+                driveIndex = drives.FirstOrDefault(x => x.VolumeLabel == sf.BackupDriveLabel)?.Index ?? drives.First().Index; 
+                //await LoadFoldersAsync();
+            }
+            await dialogue.ShowAsync(onClose);
+        }
+        //private async Task OnDriveChanged(int index)
+        //{
+        //    await LoadFoldersAsync();
+        //}
+
+        //private async Task LoadFoldersAsync()
+        //{
+        //    driveFolders = Enumerable.Empty<string>();
+        //    var selectedDrive = drives.Single(x => x.Index == driveIndex);
+        //    var war = await backupService.GetDriveFolders(selectedDrive.VolumeLabel);
+        //    if (war.Success)
+        //    {
+        //        driveFolders = war.Data;
+        //    }
+        //}
+
+        private async Task OnOK()
+        {
+            var isValid = dialogue.ValidateAnnotations();
+            if (isValid)
+            {
+                Model.OwnerId = Owner.Id;
+                Model.BackupDriveLabel = drives.Single(x => x.Index == driveIndex).VolumeLabel;
+                var r = await backupService.AddOrUpdateBackupSourceFolder(Model);
+                if (!r.Success)
+                {
+                    dialogue.AddErrors(r.Errors);
+                }
+                else
+                {
+                    dialogue.Close(DialogResult.Success(r.Data));
+                }
+            }
+        }
+        private void OnCancel()
+        {
+            dialogue.Close(DialogResult.Cancelled());
+        }
+        private async Task OnDelete()
+        {
+            var r = await backupService.DeleteBackupSourceFolderAsync(Model);
+            if (r.Success)
+            {
+                await mb.ShowAsync($"Source folder {Model.DisplayName} deleted", (dr) =>
+                {
+                    dialogue.Close(DialogResult.Success());
+                });
+            }
+            else
+            {
+                // site did not delete
+                var error = r.Errors.Values.First().First();
+                await mb.ShowAsync(
+                    new string[] {
+                    $"Source folder {Model.DisplayName} could not be deleted",
+                    $"{error}" });
+            }
+        }
+    }
+}
---
+++ Fastnet.Agents/Client/Pages/Backups/Backups.razor.cs
@@ -0,0 +1,103 @@
+﻿using Fastnet.Agents.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fastnet.Agents.Client.Pages.Backups
+{
+    public partial class Backups
+    {
+
+        [Parameter] public string Name { get; set; }
+        private bool isBusy;
+        private BackupSourceForm bsf;
+        private OwnerDTO Owner { get; set; }
+        private Dictionary<BackupSourceFolderDTO, bool> toggleStates = new();
+        protected override async Task OnParametersSetAsync()
+        {
+            await LoadOwnerAsync();
+        }
+
+        private async Task LoadOwnerAsync()
+        {
+            var result = await backupService.GetOwner(Name);
+            if(result.Success)
+            {
+                Owner = result.Data;
+                Console.WriteLine($"");
+            }
+        }
+        private async Task ShowFormAsync(BackupSourceFolderDTO sf = null)
+        {
+            await bsf.ShowAsync(sf, async (dr) => {
+                if (!dr.IsCancelled)
+                {
+                    await LoadOwnerAsync();
+                    StateHasChanged();
+                }
+            });
+        }
+        private async Task AddSourceFolderAsync()
+        {
+            //bsf.SetModel(new BackupSourceFolderDTO());
+            await ShowFormAsync();
+        }
+        private async Task EditSourceFolderAsync(BackupSourceFolderDTO item)
+        {
+            //bsf.SetModel(item);
+            await ShowFormAsync(item);
+        }
+        private string GetToggleStateClass(BackupSourceFolderDTO item)
+        {
+            return GetToggleState(item) ? "oi-expand-up" : "oi-expand-down";
+        }
+        private bool GetToggleState(BackupSourceFolderDTO item)
+        {
+            if (!toggleStates.ContainsKey(item))
+            {
+                return false;
+            }
+            else
+            {
+                return toggleStates[item];
+            }
+        }
+        private void ToggleSourceFolder(BackupSourceFolderDTO item)
+        {
+            if(!toggleStates.ContainsKey(item))
+            {
+                toggleStates.Add(item, true);
+            }
+            else
+            {
+                toggleStates[item] = !toggleStates[item];
+            }
+        }
+        private async Task DeleteBackupAsync(BackupSourceFolderDTO sf,  BackupDTO backup)
+        {
+            isBusy = true;
+            var result = await backupService.DeleteBackupAsync(backup);
+            if(result.Success)
+            {
+                List<BackupDTO> list = sf.Backups.ToList();
+                var index = list.FindIndex(x => x.Id == backup.Id);
+                list[index] = result.Data;
+                sf.Backups = list;
+                StateHasChanged();
+            }
+            isBusy = false;
+        }
+        //private async Task DownloadBackupAsync(BackupDTO backup)
+        //{
+        //    var result = await backupService.DownloadBackup2(backup);
+        //    if(result.Success)
+        //    {
+        //        ZipDownload zdl = result.Data;
+        //        await test.SaveAsFile(zdl.Filename, zdl.Base64Data);
+        //    }
+
+        //}
+    }
+}
